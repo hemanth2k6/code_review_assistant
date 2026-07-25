37f56059686684ee150f20145ec2a5d275530e4a
hemanth2k6 <hemanthkalapati2006@gmail.com>
2026-07-25 16:30:15 +0000

    feat: wire webhook handler to Github service for context processing

--- app/api/webhooks.py
+++ app/api/webhooks.py
@@ -28,9 +28,21 @@ async def handle_webhook(request:Request,x_hub_signature_256:str=Header(None)):
         raise HTTPException(status_code=400,detail="Invalid Json format")
     if event_type == "pull_request":
         action = payload.get("action")
-        print(f"Received a Pull Request event! Action: {action}")
-        return {"status": "success","message": f"Pull request event '{action}'"}
+        pr_number = payload["pull_request"]["number"]
+        repo_full_name = payload["repository"]["full_name"]
+        installation_id = payload["installation"]["id"]
+        print(f"Received a Pull Request event! Action: {action} on {repo_full_name} PR #{pr_number}")
+        if action in ["opened", "synchronize"]:
+            from app.services.github import get_pr_diff, extract_changed_files_from_diff, get_file_context
+            diff_text = await get_pr_diff(repo_full_name, pr_number, installation_id)
+            changed_files = extract_changed_files_from_diff(diff_text)
+            print(f"Files changed in this PR: {changed_files}")
+            if changed_files:
+                first_file = changed_files[0]
+                file_context = await get_file_context(repo_full_name, first_file, installation_id)
+                print(f"Successfully fetched context for {first_file} Length: {len(file_context)} characters")
+        return {"status": "success","message":f"Pull request event '{action}' processed"}
     if event_type == "ping":
-        print("GitHub sent a ping to test the connection!")
-        return {"status": "success", "message": "Pong"}
+        print("Github sent a ping to test the connection")
+        return {"status": "success","message": "Pong"}
     return {"status": "ignored", "message": f"Event '{event_type}' ignored"}
---
+++ app/services/github.py
@@ -0,0 +1,57 @@
+import jwt
+import time
+import httpx
+from app.core.config import settings
+import re
+import base64
+
+def generate_github_jwt()->str:
+    payload={
+        "iat": int(time.time()),
+        "exp": int(time.time()) + 600,
+        "iss": settings.github_app_id
+    }
+    encoded_jwt = jwt.encode(payload, settings.github_private_key, algorithm="RS256")
+    return encoded_jwt
+async def get_installation_token(installation_id: int) -> str:
+    jwt_token = generate_github_jwt()
+    headers = {
+        "Authorization" : f"Bearer {jwt_token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    url = f"https://api.github.com/app/installations/{installation_id}/access_tokens"
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        return data["token"]
+async def get_pr_diff(repo_full_name: str,pr_number: int, installation_id: int) -> str:
+    token = await get_installation_token(installation_id)
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/vnd.github.v3.diff"
+    }
+    url = f"https://api.github.com/repos/{repo_full_name}/pulls/{pr_number}"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        response.raise_for_status()
+        return response.text
+
+async def get_file_context(repo_full_name: str, file_path: str,installation_id: int) -> str:
+    token = await get_installation_token(installation_id)
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    url = f"https://api.github.com/repos/{repo_full_name}/contents/{file_path}"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        if response.status_code == 404:
+            return "File not found or was deleted"
+        response.raise_for_status()
+        data = response.json()
+        file_path = base64.b64decode(data["content"]).decode("utf-8")
+        return file_path
+def extract_changed_files_from_diff(diff_text: str) -> list:
+    file_paths = re.findall(r'^\+\+\+ b/(.+)$', diff_text, re.MULTILINE)
+    return file_paths
