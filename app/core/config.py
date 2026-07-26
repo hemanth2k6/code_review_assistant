2c49cb46700423c6368fd8ce610399d0eacf97fd
hemanth2k6 <hemanthkalapati2006@gmail.com>
2026-07-26 13:10:31 +0000

    feat: add max_tokens

--- app/core/config.py
+++ app/core/config.py
@@ -4,5 +4,6 @@ class Settings(BaseSettings):
     github_private_key:str
     webhook_secret:str
     google_api_key: str
+    max_tokens: int
     model_config=SettingsConfigDict(env_file=".env",env_file_encoding="utf-8")
 settings=Settings()
